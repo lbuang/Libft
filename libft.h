61f3467ca481b4e6c82ab073305fffd59ac9f40b
Lucky BUANG <lbuang@c5r5s11.wethinkcode.co.za>
2019-05-30 17:01:33 +0200

    libft.h

---
+++ libft.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 16:46:47 by lbuang            #+#    #+#             */
+/*   Updated: 2019/05/30 17:00:52 by lbuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct        s_list
+{
+    void            *content;
+    size_t            content_size;
+    struct s_list    *next;
+}                    t_list;
+
+int	ft_atoi(const char *str);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	isprint(int c);
+void	ft_putchar(char c);
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
+char	*ft_strchr(const char *s, int c);
+int	ft_strcmp(char *s1, char *s2);
+char	*ft_strcpy(char *dst, const	char *src);
+size_t	ft_strlen(const char *s);
+int	ft_strncmp(char *s1, char *s2, size_t n);
+char	*ft_strncpy(char *dst, const	char *src, size_t len);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+#endif
