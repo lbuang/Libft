941da8ebf553334b3fa4c937277bc0c47d42c28f
Lucky BUANG <lbuang@c5r5s11.wethinkcode.co.za>
2019-05-31 11:26:10 +0200

    update

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,11 +6,11 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:27:28 by lbuang            #+#    #+#             */
-/*   Updated: 2019/05/21 14:04:45 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/05/31 11:25:54 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 size_t	ft_strlen(const char *s)
 {
