9087340954e0245a1bbfda0b82b3183c2e866ff1
Lucky BUANG <lbuang@c5r5s11.wethinkcode.co.za>
2019-05-31 12:10:32 +0200

    update

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,11 +6,11 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 12:06:02 by lbuang            #+#    #+#             */
-/*   Updated: 2019/05/24 15:16:03 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/05/31 12:04:11 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
