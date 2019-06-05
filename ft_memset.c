a55161cb2fb39cc2c91f248cccba7a409b9dc5c2
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-05 15:36:24 +0200

    update

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 15:13:57 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/05 15:17:07 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/05 15:35:50 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memeset(void *b, int c, size_t n)
+void	*ft_memset(void *b, int c, size_t n)
 {
 	char	*p;
 
