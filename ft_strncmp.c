3a82bb5eee254d075447b14f0627a3f0d0df931d
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-05 16:20:56 +0200

    update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,11 +6,11 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 10:15:27 by lbuang            #+#    #+#             */
-/*   Updated: 2019/05/29 10:54:39 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/05 16:20:36 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int	ft_strncmp(char *s1, char *s2, size_t n)
 {
