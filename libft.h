c6a92d99e199dc616eb28c444259c80303b165ec
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-06 14:07:26 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 16:46:47 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/06 13:46:34 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/06 14:07:12 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,6 @@ void				*ft_memcpy(void *dst, const void *src, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memset(void *b, int c, size_t n);
 char				*ft_strdup(const char *s1);
+void				ft_bzero(void *s, size_t n);
 
 #endif
