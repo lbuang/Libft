ccc520c3af45df96b9ec6c14a5f027fe1615472b
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-06 15:05:50 +0200

    update

---
+++ ft_memmove.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 14:21:14 by lbuang            #+#    #+#             */
+/*   Updated: 2019/06/06 15:05:21 by lbuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*tmp;
+
+	tmp = (char)malloc(sizeof(char) * len);
+	if (tmp == NULL)
+		return (NULL);
+	ft_memcpy(tmp, src, len);
+	ft_memcpy(dst, tmp, len);
+	free(tmp);
+	return (dst);
+}
