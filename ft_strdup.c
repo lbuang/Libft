20de52f15bf351b3fde3cb16555a11b3a8e27688
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-06 13:43:13 +0200

    update

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:47:00 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/06 11:07:36 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/06 13:42:27 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,18 @@
 
 char	*ft_strdup(const char *s1)
 {
-	int			org_size;
-	static char	*dup;
-	char		*dup_offset;
+	char	*str;
+	size_t	i;
 
-	org_size = strlen(s1);
-	dup = (char *)malloc(sizeof(char) * org_size + 1);
-	if
-		(dup == NULL)
-		return ((char *)NULL);
-	dup_offset = dup;
-	while (*s1)
+	i = 0;
+	str = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
 	{
-		*dup_offset = *s1;
-		dup_offset++;
-		s1++;
+		str[i] = s1[i];
+		i++;
 	}
-	*dup_offset = '\0';
-	return (dup);
+	str[i] = '\0';
+	return (str);
 }
