266a06506b18e35e3a6e94c79fd65bc64fa30853
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-06 15:22:40 +0200

    update

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 14:21:14 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/06 15:13:08 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/06 15:22:12 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,5 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		return (NULL);
 	ft_memcpy(tmp, src, len);
 	ft_memcpy(dst, tmp, len);
-	free(tmp);
 	return (dst);
 }
