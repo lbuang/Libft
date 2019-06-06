676bed9e8b731829f9728fe73f39da38fb6f6a81
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-06 17:29:34 +0200

    update

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,23 +6,11 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:41:59 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/06 17:21:56 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/06 17:24:36 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	ft_putendl_fd(char const *s, int fd)
-{
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
-}#include "libft.h"
-
-void	ft_putendl_fd(char const *s, int fd)
-{
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
-}#include "libft.h"
 
 void	ft_putendl_fd(char const *s, int fd)
 {
