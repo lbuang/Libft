c4d60a953265da46e380c7ba2fc7e320131ab27a
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-10 16:49:59 +0200

    update

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,19 +6,19 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 15:14:48 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/10 16:48:55 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/10 16:49:49 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memalloc(size_t n)
+void	*ft_memalloc(size_t size)
 {
 	void	*m;
 	
 	unsigned int	i;
 	if (m == NULL)
 		return (NULL);
-	ft_bzero(m, n);
+	ft_bzero(m, size);
 	return (m);
 }
