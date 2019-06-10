e1d8114b3ada3b21a4eda39c471efff79be4185a
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-10 17:33:18 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 16:46:47 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/10 16:50:33 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/10 17:33:04 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,5 +56,6 @@ void				ft_memdel(void **ap);
 void				ft_strdel(char **as);
 char				*ft_strnew(size_t size);
 void				ft_strclr(char *s);
+void				ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
