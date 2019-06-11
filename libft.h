38b5fc9e96c0396cf682678b879781737023aa56
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-11 11:28:36 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 16:46:47 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/11 11:00:15 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/11 11:28:17 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ void				ft_striter(char *s, void (*f)(char*));
 char				*ft_strmap(char const *s, char (*f)(char));
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int					ft_strequ(char const *s1, char const *s2);
+void				ft_putnbr(int n);
 
 #endif
