6e766146a8a3026253c6adab6838acbcfed4f165
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-11 10:49:54 +0200

    update

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,18 +6,22 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:17:25 by lbuang            #+#    #+#             */
-/*   Updated: 2019/05/31 11:23:40 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/11 10:49:33 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	int	i;
+	size_t			i;
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
+	while (ptr1[i] == ptr2[i] && ptr1[i] != '\0' && ptr2[i] != '\0')
 		i++;
-	return (s1[i] - s2[i]);
+	return (ptr1[i] - ptr2[i]);
 }
