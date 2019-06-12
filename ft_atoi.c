a7dfddef2562912a207310af5d0a79d99dd0b056
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-12 10:52:22 +0200

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 10:56:21 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/12 10:02:53 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/12 10:51:49 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,24 @@
 
 int	ft_atoi(const char *str)
 {
-	int sign;
-	int result;
-	int i;
-
+	int	sign;
+	int	result;
+	int	i;
+	
 	i = 0;
-	sign = 1;
 	result = 0;
+	sign = 1;
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\v' \
-			|| str[i] == '-' || str[i] == '+')
+			|| str[i] == '\n' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		sign = -1;
+		if (str[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
 	}
-	i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + str[i] - '0';
