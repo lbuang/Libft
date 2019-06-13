e6eac6f1b3568c640ae25c0f3591b079e2efdae0
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-13 14:43:45 +0200

    update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:27:35 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/11 16:34:17 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/13 14:43:30 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1)
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str == NULL)
 		return (NULL);
