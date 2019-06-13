934d227873a5b5c4f2ff73ab9c4d4d677749b63d
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-13 14:36:25 +0200

    update

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 17:14:13 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/10 17:29:23 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/13 14:35:32 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 
 	i = 0;
+	if (!s)
+		return ;
 	while (s[i] != '\0')
 	{
 		f(i, s + i);
