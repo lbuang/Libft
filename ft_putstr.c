835ee1fe2c9488c9a03838d650f6cc6d05616844
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-13 14:45:19 +0200

    update

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,17 +6,19 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 11:48:15 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/11 11:51:32 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/13 14:44:50 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft"
+#include "libft.h"
 
 void	ft_putstr(const char *s)
 {
 	int i;
 
 	i = 0;
+	if (!s)
+		return ;
 	while (s[i] != '\0')
 	{
 		ft_putchar(s[i]);
