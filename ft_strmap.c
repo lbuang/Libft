9807a5b506e8c55795e40fb2f9019050e7db7555
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-13 14:37:51 +0200

    update

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 10:22:18 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/11 10:32:55 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/13 14:37:30 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	char			*str;
 
 	i = 0;
+	if (!s)
+		return ;
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
 	if (str == NULL)
 		return (NULL);
