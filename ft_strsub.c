16265e8767a4a8465b0eb9babf1ecd19071d683e
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-13 15:10:10 +0200

    update

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:02:47 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/13 14:42:30 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/13 15:09:57 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	if (!s)
-		return ;
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (NULL);
