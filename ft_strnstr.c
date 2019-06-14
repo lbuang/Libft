cf0558d75840a59a275da20c3a08cf1109d5d32c
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-14 11:40:11 +0200

    update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/13 13:28:44 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/13 13:31:44 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/14 11:39:45 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
 	size_t	i;
 	size_t	k;
+	char	*p1;
+	char	*p2;
 
+	p1 = (char *)s1;
+	p2 = (char *)s2;
 	i = 0;
+	if (p2[i] == '\0')
+		return (p1);
 	while (i < len && s1[i] != '\0')
 	{
 		k = 0;
