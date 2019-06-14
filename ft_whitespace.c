a06f46623671fd9ac90ba140233c24a57a69edc7
Lucky BUANG <lbuang@c5r6s11.wethinkcode.co.za>
2019-06-14 16:05:46 +0200

    update

--- ft_whitespace.c
+++ ft_whitespace.c
@@ -6,13 +6,13 @@
 /*   By: lbuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 15:52:04 by lbuang            #+#    #+#             */
-/*   Updated: 2019/06/14 16:02:42 by lbuang           ###   ########.fr       */
+/*   Updated: 2019/06/14 16:05:39 by lbuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	ft_whitespace(char c)
+void	ft_whitespace(char c)
 {
 	return (c == ' ' || c == '\t' || c == '\n');
 }
